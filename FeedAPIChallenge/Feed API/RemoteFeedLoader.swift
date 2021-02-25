3175c9f82a0c4c045d81fd0ddb59b9f3c88dcb73
Marko Tribl <triblmarko@gmail.com>
2021-02-25 23:47:59 +0100

    Delivers invalid data error on 200 HTTP response with invalid json

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -19,14 +19,53 @@ public final class RemoteFeedLoader: FeedLoader {
 	}
 	
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
-		client.get(from: url) { result in
+		client.get(from: url) { [weak self] result in
+
+			guard self != nil else { return }
+			
 			switch result {
-			case .success((_, _)):
-				completion(.failure(Error.invalidData))
-			case .failure(_):
+			case let .success((data, response)):
+				completion(FeedImageMapper.map(data, response))
+			case .failure:
 				completion(.failure(Error.connectivity))
 			}
 			
+		}
+	}
+}
+
+private class FeedImageMapper {
+	private struct Root: Decodable {
+		var items: [Item]
+		
+		var feed: [FeedImage] {
+			return items.map { $0.feedImage }
+		}
+	}
+	
+	private struct Item: Decodable {
+		let id: UUID
+		let description: String?
+		let location: String?
+		let imageURL: URL
+		
+		enum CodingKeys: String, CodingKey {
+			case id = "image_id"
+			case description = "image_desc"
+			case location = "image_loc"
+			case imageURL = "image_url"
+		}
+		
+		var feedImage: FeedImage {
+			return FeedImage(id: id, description: description, location: location, url: imageURL)
+		}
+	}
+	
+	static func map(_ data: Data, _ response: HTTPURLResponse) -> RemoteFeedLoader.Result {
+		if let root = try? JSONDecoder().decode(Root.self, from: data), response.statusCode  == 200 {
+			return .success(root.feed)
+		} else {
+			return .failure(RemoteFeedLoader.Error.invalidData)
 		}
 	}
 }
